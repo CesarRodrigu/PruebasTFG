8068713db1a6d1989163d66eb46b61398c9b9d1f
César Rodríguez Vilagrá <128821412+CesarRodrigu@users.noreply.github.com>
2025-05-18 22:23:47 +0200

    a

--- sql/data.sql
+++ sql/data.sql
@@ -55,7 +55,7 @@ INSERT INTO WebDB.roles (id, role_name)
 VALUES (2, 'ROLE_USER');
 INSERT INTO WebDB.users (id, created, first_name, last_name, password, username)
 VALUES (1, '2025-05-08 10:17:01.452542', 'Nombre', 'apellido',
-        '$2a$10$FZ0jQQ8NGlibQfT0B5VKQele4C0P5epzrUM70Bycfx.JWFIWyI0lC', 'admin');
+        '$2a$10$QMAJrLsmAMxiVqTOyDkQo.p5vZrkAdO142SUqSl0ry3oYxiv59nWu', 'admin');
 
 INSERT INTO WebDB.users_roles (roles_id, users_id)
 VALUES (1, 1);
