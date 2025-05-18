c330368d4d1b74f0e78bff3ccd35d36cd2457e4e
César Rodríguez Vilagrá <128821412+CesarRodrigu@users.noreply.github.com>
2025-05-18 22:32:39 +0200

    a

--- pre_containers.sh
+++ pre_containers.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+
 SRC_DIR="docs/pdfs"
 
 DEST_DIR="app/src/main/resources/static/pdfs"
 
 mkdir -p "$DEST_DIR"
 
-cp "$SRC_DIR/memoria.pdf" "$DEST_DIR/"
-cp "$SRC_DIR/anexos.pdf" "$DEST_DIR/"
+for file in memoria.pdf anexos.pdf; do
+  if [ -f "$DEST_DIR/$file" ]; then
+  else
+    cp "$SRC_DIR/$file" "$DEST_DIR/"
+  fi
+done
+
+
 export $(sed -e 's/\r$//' -e '/^#/d' -e '/^$/d' .env | xargs)
 
 FILE="./sql/data.sql"
@@ -23,3 +30,6 @@ EOF
 cat "$FILE" >> "$TMPFILE"
 
 mv "$TMPFILE" "$FILE"
+
+chmod 644 FILE
+
